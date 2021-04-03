718a231e618d1f1d6c0e7a92bba951bc842669f1
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-03 11:44:11 +1100

    fix rename to avoid circular import error

--- brain_games/scripts/brain_games.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pythonmak
-
-def main():
-    print('Welcome to the Brain Games!')
-
-
-if __name__ == '__main__':
-    main()
---
+++ brain_games/scripts/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Empty docstring."""
+
+from brain_games import cli
+
+
+def main():
+    cli.welcome_user()
+
+
+if __name__ == '__main__':
+    main()
