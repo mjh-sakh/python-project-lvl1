34fea76ef59cb26c89df8e2496920d72cdefe755
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-09 10:36:33 +1100

    refactor consolidate all prints in one place

--- brain_games/game_core.py
+++ brain_games/game_core.py
@@ -14,8 +14,8 @@ def run_game(game_engine):
         game_engine: package
     """
     name = ask_player_name()
-    welcome_player(name)
-    print(game_engine.RULES)  # noqa: WPS421
+    show_message(f'{name}! Welcome to the Brain Games!')
+    show_message(game_engine.RULES)
     generate_tasks_for_player(game_engine.task_answer_generator, name)
 
 
@@ -30,24 +30,26 @@ def generate_tasks_for_player(task_answer_generator, name):
     counter_correct_answers = 0
     while counter_correct_answers < REQUIRED_WIN_COUNT:
         task, correct_answer = task_answer_generator()
-        print(f'Question: {task}')  # noqa: WPS421, WPS305
+        show_message(f'Question: {task}')  # noqa: WPS305
         answer = ask_answer_from_player()
         if answer == correct_answer:
             counter_correct_answers += 1
-            print('Correct!')  # noqa: WPS421
+            show_message('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: WPS421, WPS305, E501
-            print(f"Let's try again, {name}!")  # noqa: WPS421, WPS305
+            show_message(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: WPS305, E501
+            show_message(f"Let's try again, {name}!")  # noqa: WPS305
             break
     else:
-        print(f'Congratulations, {name}!')  # noqa: WPS421, WPS305
+        show_message(f'Congratulations, {name}!')  # noqa: WPS305
 
 
-def welcome_player(name):
+def show_message(message):
     """
-    Welcome player by name.
+    Display given message to users.
+
+    Currently it uses print to cli.
 
     Args:
-        name: str
+        message: str
     """
-    print(f'{name}! Welcome to the Brain Games!')  # noqa: WPS421, WPS305
+    print(message)  # noqa: WPS421
