8ed4c68f7441405dadfdf4ea461cb77d166fd9d9
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-08 20:06:38 +1100

    refactor while loop

--- brain_games/game_core.py
+++ brain_games/game_core.py
@@ -27,7 +27,7 @@ def run_game_engine(game_engine, name):
         name: players name
     """
     counter_correct_answers = 0
-    while True:
+    while counter_correct_answers < REQUIRED_WIN_COUNT:
         task, correct_answer = game_engine()
         print(f'Question: {task}')  # noqa: WPS421, WPS305
         answer = prompt.string('Your answer: ')
@@ -38,6 +38,5 @@ def run_game_engine(game_engine, name):
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: WPS421, WPS305, E501
             print(f"Let's try again, {name}!")  # noqa: WPS421, WPS305
             break
-        if counter_correct_answers == REQUIRED_WIN_COUNT:
-            print(f'Congratulations, {name}!')  # noqa: WPS421, WPS305
-            break
+    else:
+        print(f'Congratulations, {name}!')  # noqa: WPS421, WPS305
