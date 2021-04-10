0f78af2eba5689e74c6aeb899b0edee30b37bb10
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-10 20:52:26 +1100

    refactor merge two methods

--- brain_games/game_core.py
+++ brain_games/game_core.py
@@ -17,20 +17,9 @@ def run_game(game_engine):
     name = get_input_from_player('May I have your name?\n')
     show_message(f'{name}! Welcome to the Brain Games!')  # noqa: WPS305
     show_message(game_engine.RULES)
-    generate_tasks_for_player(game_engine.task_answer_generator, name)
-
-
-def generate_tasks_for_player(task_answer_generator, name):
-    """
-    Start game play.
-
-    Args:
-        task_answer_generator: function that returns str, str
-        name: players name
-    """
     counter_correct_answers = 0
     while counter_correct_answers < REQUIRED_WIN_COUNT:
-        task, correct_answer = task_answer_generator()
+        task, correct_answer = game_engine.task_answer_generator()
         show_message(f'Question: {task}')  # noqa: WPS305
         answer = get_input_from_player('Your answer: ')
         if answer == correct_answer:
