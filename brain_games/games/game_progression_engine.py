ff20b977c1aeb409070eb99b15759bbbd3441aab
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-10 20:52:10 +1100

    refactor move series generation in separte func

--- brain_games/games/game_progression_engine.py
+++ brain_games/games/game_progression_engine.py
@@ -4,7 +4,23 @@ import random
 RULES = 'What number is missing in the progression?'
 
 
-def task_answer_generator():
+def populate_series(series, initial_value: int, increment: int, length: int = 10):  # noqa: E501
+    """
+    Populate given series in place with values starting from initial value.
+
+    Args:
+        series: list
+        initial_value: int
+        increment: int
+        length: int
+
+    """
+    series.append(initial_value)
+    for _ in range(length - 1):
+        series.append(series[-1] + increment)
+
+
+def task_answer_generator():  # noqa: WPS210
     """
     Generate task and answer for progression game.
 
@@ -16,9 +32,10 @@ def task_answer_generator():
         increment = random.randint(-11, 11)  # noqa: S311, WPS432
         if increment != 0:
             break
-    series = [random.randint(40, 70)]  # noqa: S311, WPS432
-    for _ in range(random.randint(6, 10)):  # noqa: S311, WPS432
-        series.append(series[-1] + increment)
+    initial_value = random.randint(40, 70)  # noqa: S311, WPS432
+    series_length = random.randint(6, 10)  # noqa: S311, WPS432
+    series = []
+    populate_series(series, initial_value, increment, series_length)
     ix = random.randint(0, len(series) - 1)  # noqa: S311
     lookup_element = series[ix]
     series[ix] = '..'
