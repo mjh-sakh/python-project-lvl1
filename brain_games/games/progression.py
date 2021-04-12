1f7f958780f5ed78079541606d9850f112ac18a3
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-12 21:48:40 +1100

    refactor separate progression generation function and renaming

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,6 +4,21 @@ import random
 RULES = 'What number is missing in the progression?'
 
 
+def generate_progression(initial_value, increment, progression_length):
+    """
+    Generate and returns progression list.
+
+    Args:
+        initial_value: int
+        increment: int
+        progression_length: int
+
+    Returns:
+         progression: list
+    """
+    return list(map(lambda x: initial_value + x * increment, range(progression_length)))  # noqa: WPS221, WPS111, E501
+
+
 def generate_task_answer():  # noqa: WPS210
     """
     Generate task and answer for progression game.
@@ -17,10 +32,10 @@ def generate_task_answer():  # noqa: WPS210
         if increment != 0:
             break
     initial_value = random.randint(40, 70)  # noqa: S311, WPS432
-    series_length = random.randint(6, 10)  # noqa: S311, WPS432
-    series = list(map(lambda x: initial_value + x * increment, range(series_length)))  # noqa: WPS221, WPS111, E501
-    lookup_element_ix = random.randint(0, len(series) - 1)  # noqa: S311
-    lookup_element = series[lookup_element_ix]
-    series[lookup_element_ix] = '..'
-    progression = ' '.join(map(str, series))
-    return progression, str(lookup_element)
+    progression_length = random.randint(6, 10)  # noqa: S311, WPS432
+    progression = generate_progression(initial_value, increment, progression_length)  # noqa: WPS221, WPS111, E501
+    lookup_element_ix = random.randint(0, len(progression) - 1)  # noqa: S311
+    lookup_element = progression[lookup_element_ix]
+    progression[lookup_element_ix] = '..'
+    progression_string = ' '.join(map(str, progression))
+    return progression_string, str(lookup_element)
