956fafedf35286944a7fe7673066fbcccb546891
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-04-13 20:22:57 +1100

    refactor lint

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,7 +4,7 @@ import random
 RULE = 'What is the result of the expression?'
 
 
-def generate_task_answer():
+def generate_task_answer():  # noqa: WPS210
     """
     Generate task and answer for cal game.
 
@@ -20,6 +20,6 @@ def generate_task_answer():
     number1 = random.randint(20, 50)  # noqa: S311, WPS111, WPS432
     number2 = random.randint(1, 20)  # noqa: S311, WPS111, WPS432
     operator = random.choice(list(operators.keys()))  # noqa: S311
-    task = str(number1) + ' ' + operator + ' ' + str(number2)
+    task = f'{number1} {operator} {number2}'
     answer = str(operators[operator](number1, number2))
     return task, answer
